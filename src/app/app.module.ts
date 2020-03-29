d87f77d32ad408ea4b25ef6cef2ec70a46ce7275
Emil Lehti <29.lehti@gmail.com>
2020-03-29 12:29:29 +0200

    Resizeable and dynamic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,12 @@ import { TableComponent } from './table/table.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,7 +21,12 @@ import { MatTableModule } from '@angular/material/table';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatTableModule
+    MatTableModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,36 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { MatCheckboxChange } from '@angular/material/checkbox';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+export interface Column {
+  key: string;
+  isVisible: boolean;
 }
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
+
 export class TableComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[];
+  inputValue: string;
+
+  allColumnsInfo: Column[] = [
+    { key: 'position', isVisible: true},
+    { key: 'name', isVisible: true},
+    { key: 'weight', isVisible: false},
+    { key: 'symbol', isVisible: true},
+    { key: 'mass', isVisible: true},
+  ];
+
+  dataSource = [
+    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', mass: 12},
+    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', mass: 3},
+    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', mass: 45},
+    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', mass: 678},
+    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B', mass: 90},
+    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C', mass: 64},
+    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N', mass: 5},
+    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O', mass: 8},
+    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F', mass: 33},
+    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne', mass: 89},
+  ];
   constructor() { }
 
   ngOnInit(): void {
+    this.updateDisplayedColumns();
   }
 
+  toggleColumn(event: MatCheckboxChange) {
+    this.allColumnsInfo.forEach(column => {
+      if (column.key === event.source.value) {
+        column.isVisible = !column.isVisible;
+        return;
+      }
+    });
+    this.updateDisplayedColumns();
+  }
+
+  onClickSubmit(formData: any) {
+    this.allColumnsInfo.push({ key: formData.columnName, isVisible: true });
+    this.dataSource.map(d => {
+      d[formData.columnName] = Math.floor(Math.random() * 100);
+    });
+    this.updateDisplayedColumns();
+    this.inputValue = '';
+ }
+
+ updateDisplayedColumns() {
+  this.displayedColumns = this.allColumnsInfo.filter(c => c.isVisible).map(c => c.key);
+ }
 }
