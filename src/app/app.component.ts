5ffc14baf8216feb5ea043cf9d5296b04e49c51e
Emil Lehti <29.lehti@gmail.com>
2020-08-19 15:26:52 +0200

    Revert "Merge branch 'gh-pages'"
    This reverts commit d5ca1ef098afea30955234fc9462a997127fa409, reversing
    changes made to 08ac79328e24eecd88a483a8a1b317d0e8b658eb.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { SplitFactory } from '@splitsoftware/splitio';
-import { splitioKey } from '../../secrets';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'tabletest';
-  enableElectricCurtain: boolean;
-  brewCoffee: boolean;
-
-  constructor() {}
-
-  ngOnInit() {
-    const factory = SplitFactory({
-      core: {
-        authorizationKey: splitioKey,
-        key: 'CUSTOMER_ID'
-     }
-    });
-
-    const client = factory.client();
-
-    client.on(client.Event.SDK_READY, () => {
-      const brew = client.getTreatment('brew-coffee');
-      if (brew) {
-        this.brewCoffee = true;
-      } else {
-        this.brewCoffee = false;
-      }
-
-    });
-  }
-
 }
---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TableComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    DragDropModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
